5677673ea49223f3f2ebc431e48a172fd2490b24
Kelvin <7710340+kOchirasu@users.noreply.github.com>
2022-01-09 19:48:51 -0800

    Add ItemDescription

--- Maple2.File.Generator.Debugger/Program.cs
+++ Maple2.File.Generator.Debugger/Program.cs
@@ -1,12 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿namespace Maple2.File.Generator.Debugger;
 
-using Maple2.File.Generator.Debugger;
+public static class Program {
+    public static void Main(string[] args) {
+        Console.WriteLine("Hello, World!");
+        var d = new BackingField();
+        Console.WriteLine($"BEFORE: {d.duplicate}");
+        d.Field = "updated";
+        Console.WriteLine($"AFTER: {d.duplicate}");
 
-Console.WriteLine("Hello, World!");
-var d = new BackingField();
-Console.WriteLine($"BEFORE: {d.duplicate}");
-d.Field = "updated";
-Console.WriteLine($"AFTER: {d.duplicate}");
-
-Enum.Parse<TestEnum>("A");
-string s = TestEnum.A.ToString();
+        Enum.Parse<TestEnum>("A");
+        string s = TestEnum.A.ToString();
+    }
+}
---
+++ Maple2.File.Parser/Xml/String/ItemDescription.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using System.Xml.Serialization;
+using M2dXmlGenerator;
+
+namespace Maple2.File.Parser.Xml.String;
+
+[XmlRoot("ms2")]
+public partial class ItemDescription {
+    [M2dFeatureLocale(Selector = "id")] private IList<Key> _key;
+
+    public partial class Key : IFeatureLocale {
+        [XmlAttribute] public int id;
+        [XmlAttribute] public string tooltipDescription;
+        [XmlAttribute] public string guideDescription;
+    }
+}
