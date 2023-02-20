40b6b7ad6a01f0fd33c31eddf2d904840638ea3b
Kelvin <7710340+kOchirasu@users.noreply.github.com>
2023-02-19 16:44:29 -0800

    Set Npc `movearea` default value

--- Maple2.File.Parser/Xml/Npc/Normal.cs
+++ Maple2.File.Parser/Xml/Npc/Normal.cs
@@ -6,6 +6,6 @@ namespace Maple2.File.Parser.Xml.Npc;
 public partial class Normal {
     [M2dArray] public string[] action = {"Idle_A"};
     [M2dArray] public int[] prob = {10000};
-    [XmlAttribute] public int movearea;
+    [XmlAttribute] public int movearea = 200;
     [XmlAttribute] public string maidExpired = string.Empty;
 }
