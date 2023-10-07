96c24fc9ca890ac6d2b74b8c5c1615d0f4dd6e37
kafka <96216475+kaffination@users.noreply.github.com>
2023-10-07 15:21:09 -0600

    MKR related xml table parse scripts (#17)
    was not able to figure out a multi table parsing script but added scripts for the first two tables.

---
+++ Maple2.File.Parser/Xml/Table/SurvivalLevel.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using System.Xml.Serialization;
+using M2dXmlGenerator;
+
+namespace Maple2.File.Parser.Xml.Table;
+
+// ./data/xml/table/na/survivallevel.xml
+[XmlRoot("ms2")]
+public partial class SurvivalLevelRoot {
+    [M2dFeatureLocale(Selector = "level")] private IList<SurvivalLevel> _survivalLevelExp;
+}
+
+//<survivalLevelExp level="1" reqExp="0" grade="0" feature="SurvivalContents03" />
+//<survivalLevelExp level="60" reqExp="113760" grade="7" feature="SurvivalContents03" />
+public partial class SurvivalLevel : IFeatureLocale {
+    [XmlAttribute] public int level;
+    [XmlAttribute] public long reqExp;
+    [XmlAttribute] public byte grade;
+}
---
+++ Maple2.File.Parser/Xml/Table/SurvivalLevelReward.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using System.Xml.Serialization;
+using M2dXmlGenerator;
+
+namespace Maple2.File.Parser.Xml.Table;
+
+// ./data/xml/table/na/survivallevelreward.xml
+[XmlRoot("ms2")]
+public partial class SurvivalLevelRewardRoot {
+    [M2dFeatureLocale(Selector = "level")] private IList<SurvivalLevelReward> _survivalLevelReward;
+}
+
+//<survivalLevelReward level="1" rewardItemID1="" rewardItemRank1="" rewardItemCount1="" rewardItemID2="" rewardItemRank2="" rewardItemCount2="" rewardItemID3="" rewardItemRank3="" rewardItemCount3="" feature="SurvivalContents02" />
+//<survivalLevelReward level="300" rewardItemID1="20301724" rewardItemRank1="1" rewardItemCount1="10" rewardItemID2="" rewardItemRank2="" rewardItemCount2="" rewardItemID3="" rewardItemRank3="" rewardItemCount3="" feature="SurvivalContents02" />
+public partial class SurvivalLevelReward : IFeatureLocale {
+    [XmlAttribute] public int level;
+    [XmlAttribute] public int rewardItemID1;
+    [XmlAttribute] public short rewardItemRank1;
+    [XmlAttribute] public int rewardItemCount1;
+    [XmlAttribute] public int rewardItemID2;
+    [XmlAttribute] public short rewardItemRank2;
+    [XmlAttribute] public int rewardItemCount2;
+    [XmlAttribute] public int rewardItemID3;
+    [XmlAttribute] public short rewardItemRank3;
+    [XmlAttribute] public int rewardItemCount3;
+}
